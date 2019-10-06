fcd1708bf0a17166b3af56f5a9377c4d09f33650
lwl_glb <lwl_glb@126.com>
2019-10-06 14:19:47 +0800

    666222

--- src/main/java/testsina/A.java
+++ src/main/java/testsina/A.java
@@ -2,10 +2,7 @@ package testsina;
 
 public class A {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("666");
-=======
-		System.out.println("2");
->>>>>>> branch 'master' of https://github.com/liwanliang1988/testsina.git
+	System.out.println("666");
+	System.out.println("2");
 	}
 }
